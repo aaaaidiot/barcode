295786fc7462e837d55ddfcacfa734d252603700
MasudaTsuya <b9p31081@bunkyo.ac.jp>
2021-10-19 15:57:36 +0900

    色々改良

--- app/src/main/java/com/example/barcode/MainActivity.java
+++ app/src/main/java/com/example/barcode/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
@@ -16,17 +17,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new IntentIntegrator(this).initiateScan();
+        //ZXingのインスタンス作成
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        //横から縦向きに,ここからZxingの設定が行える
+        // 詳細は→https://github.com/journeyapps/zxing-android-embedded
+        integrator.setOrientationLocked(false);
+        integrator.initiateScan();
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode,Intent data) {
+        TextView textView = findViewById(R.id.code);
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
         if(result != null) {
             if(result.getContents() == null) {
                 Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+
             } else {
                 Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+                textView.setText(result.getContents());
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data);
